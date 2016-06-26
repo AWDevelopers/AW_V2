665c0d77f2ac752c719083b8ff0027ccb07b3220
SilviaLenFer <sillendi@ucm.es>
2016-06-26 13:58:12 +0200

    arreglado modificar producto

--- proyecto/estatica/vistaModificarProducto.php
+++ proyecto/estatica/vistaModificarProducto.php
@@ -23,13 +23,13 @@
 						<p><h2>Precio del producto:</h2></p>
 						<input type = "text"  name= "precio" value ="<?= $_REQUEST['precio']?>">
 						<p><h2>Descripción corta del producto:</h2> </p>
-						<textarea name= "descripcionCorta" rows="4" cols="40" value="<?php $_REQUEST['desc']?>"></textarea> 
+						<textarea name= "descripcionCorta" rows="4" cols="40" placeholder=<?$_REQUEST['desc']?><?= $_REQUEST['desc']?></textarea> 
 						<p><h2>Descripción larga del producto:</h2> </p>
-						<textarea  rows="10" cols="40" name= "descripcionLarga" value="<?= $_REQUEST['desl']?>"></textarea> 
+						<textarea  rows="10" cols="40" name= "descripcionLarga" placeholder=<?$_REQUEST['desl']?><?= $_REQUEST['desl']?></textarea> 
 						<p><h2>Stock</h2> </p>
 						<input type = "text" name= "stock" value ="<?= $_REQUEST['stock']?>"></input>
 						<p><h2>Imagen</h2> </p>
-						<input id="file_url" type="file" name="imagen"> (*)</input>
+						<input id="file_url" type="file" name="imagen" required> (*)</input>
 						<p><input type= "submit" value = "MODIFICAR"></p>
 						</div>
 						<div>
